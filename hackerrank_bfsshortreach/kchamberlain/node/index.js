38962dcb5707de1305a15422a113efe18a324dba
Koleok <kchamb3@gmail.com>
2016-03-18 13:16:10 -0400

    Not done :(

---
+++ hackerrank_bfsshortreach/kchamberlain/node/index.js
@@ -0,0 +1,24 @@
+const
+  _ = require('lodash'),
+  fs = require('fs')
+
+const cases = fs
+  .readFileSync('../cases.txt')
+  .toString()
+
+const
+  splitBySpace = str => _.compact(str.split(' ')),
+  splitByLine = str => str.split(/\n/),
+  parseInts = arr => _.map(arr, _.parseInt),
+  hasOneElement = arr => arr.length === 1,
+  hasTwoElements = arr => arr.length === 2,
+  parseTests = str => {
+    return _.chain(str)
+      .thru(splitByLine)
+      .compact()
+      .map(splitBySpace)
+      .map(parseInts)
+      .value();
+  }
+
+console.log(parseTests(cases));
