b990733205399be953daff1f17e0bf94dcff825c
Nicholas Tietz <ntietz@gmail.com>
2016-04-08 12:02:45 -0400

    Making it shorter!

--- hackerrank_kitty-and-katty/ntietz/tweetable.py
+++ hackerrank_kitty-and-katty/ntietz/tweetable.py
@@ -1,1 +1,1 @@
-import sys;map(lambda _:[sys.stdout.write(('Ki'if(n==1 or n%2==0)else'Ka')+'tty\n') for n in [int(raw_input())]],[0]*int(raw_input()))
+import sys;map(lambda _:[sys.stdout.write(('Ki'if(n<2 or n%2<1)else'Ka')+'tty\n')for n in[int(input())]],[0]*int(input()))
