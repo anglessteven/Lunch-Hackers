131680267bea610ed5a572cd5ffdadd80f0f81ed
Nicholas Tietz <ntietz@gmail.com>
2016-04-08 12:08:59 -0400

    Trimming it more

--- hackerrank_kitty-and-katty/ntietz/tweetable2.py
+++ hackerrank_kitty-and-katty/ntietz/tweetable2.py
@@ -1,1 +1,1 @@
-list(map(lambda _:[print(('Ki'if(n<2 or n%2<1)else'Ka')+'tty')for n in[int(input())]],[0]*int(input())))
+[[print(('Ki'if(n<2 or n%2<1)else'Ka')+'tty')for n in[int(input())]]for x in[0]*int(input())]
