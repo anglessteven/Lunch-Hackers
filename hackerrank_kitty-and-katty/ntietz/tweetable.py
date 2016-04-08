5b63a6747de632aea810917e101274c317c1961b
ntietz <ntietz@gmail.com>
2016-04-07 22:54:50 -0400

    Made that one shorter, too.

--- hackerrank_kitty-and-katty/ntietz/tweetable.py
+++ hackerrank_kitty-and-katty/ntietz/tweetable.py
@@ -1,1 +1,1 @@
-import sys;map(lambda _:[[_ for n in [int(raw_input())]],sys.stdout.write(('Ki'if(n==1 or n%2==0)else'Ka')+'tty\n')],[0]*int(raw_input()))
+import sys;map(lambda _:[sys.stdout.write(('Ki'if(n==1 or n%2==0)else'Ka')+'tty\n') for n in [int(raw_input())]],[0]*int(raw_input()))
